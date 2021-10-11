40e10e0f655734ce02cb4436564cddaf955a62fe
alireza <alireza.azari2235@outlook.com>
2021-10-11 00:25:09 -0300

    get geolocation API

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// prettier-ignore
+
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector('.form');
@@ -10,3 +10,19 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+
+//Geolocation API
+//Get current position
+navigator.geolocation.getCurrentPosition(
+    //success callback
+    function(position){
+//use destructuring ot get latitude&longitude from coords object
+const {latitude}=position.coords;
+const {longitude}=position.coords;
+    },
+    
+    //error callback
+    function(){
+    alert('Could not find your position')
+})
