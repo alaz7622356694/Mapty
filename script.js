f38fc64cfdf2fb86049dd0063b509f76a4d66ca9
Alireza <alireza.az76@gmail.com>
2021-10-17 22:44:21 -0300

    refactoring&adding class

--- script.js
+++ script.js
@@ -10,72 +10,87 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-let map , mapEvent;
+
+class App{
+    #map;
+    #mapEvent;
+constructor(){
+    this._getPosition();
+    form.addEventListener('submit',this._newWorkou.bind(this))
+    //once we submit a form a location tag appears on the map
+    //toggling between cadence and elevation field in the form  
+    inputType.addEventListener('change',this._toggleElevationField)
+}
+
+_getPosition(){
 
 //Geolocation API
 //Get current position
-navigator.geolocation.getCurrentPosition(
-    //success callback
-    function(position){
-//use destructuring to get latitude&longitude from coords object
-const {latitude}=position.coords;
-const {longitude}=position.coords;
+navigator.geolocation.getCurrentPosition(this._loadMap.bind(this),
+//error callback
+ function(){
+alert('Could not find your position')
+})
 
-/////////create a map in the 'map' div, add tiles of our choice, and then add a marker with some text in a popup////////////
-const coords=[latitude,longitude]
-
-//first argument of setview is our current coordination and the second is zoom number
-//'map' is html element id
-map = L.map('map').setView(coords, 13);
-//open street map is an open source map for free. you can change its style
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-
-//Adding event to the map object
-map.on('click',function(mapE){
-    mapEvent=mapE
+    }
+ _loadMap(position){
+       //success callback
+ 
+        //use destructuring to get latitude&longitude from coords object
+        const {latitude}=position.coords;
+        const {longitude}=position.coords;
+        
+        /////////create a map in the 'map' div, add tiles of our choice, and then add a marker with some text in a popup////////////
+        const coords=[latitude,longitude]
+        
+        //first argument of setview is our current coordination and the second is zoom number
+        //'map' is html element id
+        this.#map = L.map('map').setView(coords, 13);
+        //open street map is an open source map for free. you can change its style
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+        
+        
+        //Adding event to the map object
+        this.#map.on('click',this._showForm.bind(this))
+           
+        
+        
+        
+        
+        
+            
+    }
+_showForm(mapE){
+    this.#mapEvent=mapE
     //when we click a form appears
     //form is hidden by default so we need to remove its hidden class
-
     form.classList.remove('hidden');
+}
+_toggleElevationField(){
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+}
+ _newWorkout(e){
+        e.preventDefault();
+        inputCadence.value=inputDistance.value=inputElevation.value=inputDuration.value='';
 
+        //Clear input fields 
 
-})
+        //display marker
 
+        // Adding lat and lng of the clicked location to the map
+        const {lat , lng}=this.#mapEvent.latlng
+        L.marker([lat,lng]).addTo(this.#map)
+        //get popup options from leaflet documentation :https://leafletjs.com/reference-0.7.7.html#popup 
 
-    },
-    
-
-
-    //error callback
-    function(){
-    alert('Could not find your position')
-})
-
-// adding submit eventListener
-
-form.addEventListener('submit',function(e){
-    //once we submit a form a location tag appears on the map
-    e.preventDefault();
-    inputCadence.value=inputDistance.value=inputElevation.value=inputDuration.value='';
-
-//Clear input fields 
-
-//display marker
-
-    // Adding lat and lng of the clicked location to the map
-    const {lat , lng}=mapEvent.latlng
-    L.marker([lat,lng]).addTo(map)
-    //get popup options from leaflet documentation :https://leafletjs.com/reference-0.7.7.html#popup 
-
-    .bindPopup(L.popup({
-        maxWidth:250,
-        minWidth:100,
-        autoClose:false,
-        closeOnClick:false,
-        className:'running-popup',
+         .bindPopup(L.popup({
+             maxWidth:250,
+             minWidth:100,
+             autoClose:false,
+             closeOnClick:false,
+             className:'running-popup',
        
     }))
     .setPopupContent('Workout')
@@ -83,14 +98,13 @@ form.addEventListener('submit',function(e){
 
 
 
-})
+}
+    
 
-//toggling between cadence and elevation field in the form
+}
 
-inputType.addEventListener('change',function(){
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+const app=new App();
+// adding submit eventListener
 
 
-})
 
